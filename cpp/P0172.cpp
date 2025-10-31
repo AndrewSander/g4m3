746e78e9d8d2accb930e099eaadcdabc24241e31
Andrew Sander <andrewsanderf@gmail.com>
2025-10-31 12:45:23 -0300

    Adicionando o P0172.cpp

---
+++ cpp/P0172.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int n1; cin >> n1;
+
+    if (n1 <= 50){
+        cout << "YES" << "\n";
+    }
+    else{
+        cout << "NO" << "\n";       
+    }
+
+    return 0;
+}
