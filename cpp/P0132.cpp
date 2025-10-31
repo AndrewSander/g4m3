2760ed42fdd258f728a4783e81615a6f0ca5927e
Andrew Sander <andrewsanderf@gmail.com>
2025-10-31 12:44:11 -0300

    Adicionando o P0132.cpp

---
+++ cpp/P0132.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    string nome; cin >> nome;
+    string matricula; cin >> matricula;
+    
+    cout << "Boa prova" << "\n";
+    cout << nome << " " << matricula << "\n";
+
+    return 0;
+}
