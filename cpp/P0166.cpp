c8e3623382ecb64859983e6611a552d196d420e6
Andrew Sander <andrewsanderf@gmail.com>
2025-10-31 12:44:52 -0300

    Adicionando o P0166.cpp

---
+++ cpp/P0166.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int n1,n2; cin >> n1; cin >> n2;
+
+    if (n1 > n2){
+        cout << n1 << "\n";
+    }
+    else if (n2 > n1){
+        cout << n2 << "\n";
+    }
+    else{
+        cout << "PEGADINHA" << "\n";
+    }
+
+    return 0;
+}
