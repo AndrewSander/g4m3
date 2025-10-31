7e70b3c7e76102dae55cd476c5110d0fe31266d7
Andrew Sander <andrewsanderf@gmail.com>
2025-10-31 12:36:36 -0300

    Adicionando o P0083.cpp

---
+++ cpp/P0083.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int num; cin >> num;
+    
+    if (num > 0){
+        cout << num << "\n";
+    }
+    else{
+        cout << 0 << "\n";
+    }
+
+    return 0;
+}
